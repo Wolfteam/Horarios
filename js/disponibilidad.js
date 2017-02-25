6253a5920d61510882bfd81529e6f98f25087e42
Efrain Bastidas <mimo4325@gmail.com>
2017-02-25 14:25:18 -0400

    Agregada logica que valida si existen menos de 2 hrs academicas consecutivas por dia (lado cliente). Cambios menores a la interfaz

--- js/disponibilidad.js
+++ js/disponibilidad.js
@@ -1,4 +1,4 @@
-var horasCargadas = 0;
+var horasRestantes = 0;
 var horasACumplir;
 
 $(document).ready(function() {
@@ -8,57 +8,154 @@ $(document).ready(function() {
 	},function(data){
 		$("#selector_profesores").html(data);
 	});
-	
+
 	$("#selector_profesores").change(function(){
 		$("#selector_profesores").each(function(){
-			var cedula = $("#selector_profesores option:selected").val();
-			$.post('../controller/disponibilidad.php', {
-				cedula: cedula
-			}, function(data) {
-				var horas = $.parseJSON(data);
-				horasACumplir = horas.horas_a_cumplir;
-				$("#horas_a_cumplir").val(horasACumplir);
-				//Se debe cargar la disponibilidadque tenia el profesor
-				//en caso de tenerla, para luego ser editada 
-			});
+			if ($("#selector_profesores option:selected").val()!=0) {
+				var cedula = $("#selector_profesores option:selected").val();
+				$.post('../controller/disponibilidad.php', {
+					cedula: cedula
+				}, function(data) {
+					limpiarCeldas();
+					var horas = $.parseJSON(data);
+					horasACumplir = horas.horas_a_cumplir;
+					//debo hacer una forma de saber si ya tiene las horas cargadas (en cuyo caso
+					//horasRestantes =0 o si no las tiene, en cuyo caso horasRestantes=horasACumplir)
+					horasRestantes = horasACumplir;
+					$("#horas_a_cumplir").val(horasACumplir);
+					$("#horas_restantes").val(horasRestantes);
+					//Se debe cargar la disponibilidadque tenia el profesor
+					//en caso de tenerla, para luego ser editada 
+				});
+			}else{
+				limpiarCeldas();
+			}
 		});
 	});
-	//Las tablas son transparentes, no tienen codigo de color
-	//Debo arreglar ese problema para que pueda sumar y restar bien las horas
+	//Si el selector esta en una option diferente a la default, se permite el cambio
+	//de color de las celdas al hacer click y se actualizan las horas_restantes
 	$("td").click(function(){
-		var atributo = $(this).css("background-color");
-		console.log(atributo);
-
-		if ($(this).attr("bgcolor")=="#fff" || $(this).css("background-color")=="transparent") {
-			$(this).attr("bgcolor","#5cb85c");
-			horasCargadas++;
-		}else{
-			$(this).attr("bgcolor","#fff");
-			if (horasCargadas!=0) {
-				horasCargadas--;
-			}			
+		if ($("#selector_profesores option:selected").val()!=0) {
+			var atributo = $(this).css("background-color");
+			if ($(this).attr("bgcolor")=="#fff" || $(this).attr("bgcolor")=="#057D97"){
+				if (horasRestantes>0) {
+					$(this).attr("bgcolor","#5cb85c");
+					horasRestantes--;
+				}	
+			}else{
+				$(this).attr("bgcolor","#fff");
+				horasRestantes++;
+			}
+			$("#horas_restantes").val(horasRestantes);
 		}
-		$("#horas_restantes").val(horasPorAsignar(horasACumplir,horasCargadas));
 	});
-	//Cambia el color cuando pasas el mouse encima de una celda
-	//o sales de ella,da conflicto con lo de restar o sumar horas
-	/*
+
+	//Cambia el color cuando pasas el mouse encima de una celda o sales de ella,
+	//si y solo si la option del select es diferente a la default	
 	$("td").mouseover(function(){
-		if ($(this).attr("bgcolor")!="#5cb85c") {
-			$(this).attr("bgcolor","#057D97");
+		if ($("#selector_profesores option:selected").val()!=0) {
+			if ($(this).attr("bgcolor")!="#5cb85c") {
+				$(this).attr("bgcolor","#057D97");
+			}
 		}
 	});
 	$("td").mouseout(function(){
-		if ($(this).attr("bgcolor")!="#5cb85c") {
-			$(this).attr("bgcolor","#fff");
+		if ($("#selector_profesores option:selected").val()!=0) {
+			if ($(this).attr("bgcolor")!="#5cb85c") {
+				$(this).attr("bgcolor","#fff");
+			}
 		}
 	});
-	*/
 });
 
-function horasPorAsignar(dataA,dataB){
-	var resultado = dataA - dataB;
-
-	console.log(resultado);
-	return resultado;
+//Coloca las celdas de color blanco, puesto que inicialmente son transparentes 
+//y limpia los campos deshabilitados
+function limpiarCeldas() {
+	$("#tabla_horario tbody tr td").attr("bgcolor","#fff");
+	$("#horas_a_cumplir").val("");
+	$("#horas_restantes").val("");
 }
+
+//Esta funcion es llamada al presionar el boton de guardar
+function cargarDisponibilidad(){
+	if (horasRestantes==0 && $("#selector_profesores option:selected").val()!=0) {
+		var data = guardarData();
+		if (!validarData(data)) {
+			alert("Un dia contiene menos de 2 horas academicas consecutivas");
+		}else{
+			//aca debo implantar la parte que se pasara al servidor
+			alert("Guardado con exito");
+		}
+	}else if ($("#selector_profesores option:selected").val()==0){
+		alert("No has seleccionado a un profesor");
+	}else{
+		alert("No has asignado todas tus horas requeridas");
+	}
+	return;
+}
+
+//Esta funcion recoge la data seleccionada por el usuario y devuelve un array
+function guardarData(){
+	var arrayData=[];
+	var tabla=document.getElementById("tabla_horario");
+	for (var j = 1; j < 7; j++) {		
+		for (var i = 1; i < 13; i++) {
+			if (tabla.rows[i].cells[j].getAttribute("bgcolor")=="#5cb85c") {
+				arrayData.push(i+","+j);
+			}	
+		}
+	}
+	return arrayData;
+}
+
+//Esta funcion valida que hayan minimo 2 horas academicas seguidas en un dia
+//Recibe como parametro un array que contiene los datos de los dias-horas seleccionados
+function validarData(data){
+	var indexA,indexB,indexC,i,j,k,coincidenciasMinimas;
+	var arrayCoincidencias=[];
+	for (j = 1; j < 7; j++) {
+		var coincidencias=0;
+		var coincidenciasObtenidas = 0;		
+		for (i = 1; i < 13; i++) {
+			indexA = i+","+j;
+			for (k = 0; k <= data.length; k++) {
+				if (data[k]==indexA) {
+					coincidencias++;
+					break;
+				}	
+			}
+		}
+		//El chiste aca es que coincidencias llegue a 0, si no lo hace es porque
+		//no se cumple que hayan 2 hrs academicas consecutivas
+		//Para validar pregunto por la fila en la q me encuentro y la siguiente,
+		//si ambas estan seleccionadas resto 2 e incremento la fila para poder
+		//preguntar por las 2 siguientes. Sino se cumple esa condicion, preg. si la celda
+		//anterior esta seleccionada y si k>0 entonces resto 1. Este caso ocurre solo cuando
+		//tienes mas de 3 celdas seguidas seleccionadas y la 4ta no lo esta
+		for (i = 1; i <= 12; i++) {
+			indexA = i+","+j;
+			indexB = (i+1)+","+j;
+			indexC = (i-1)+","+j;
+			for (k = 0; k < data.length; k++) {
+				if (data[k]==indexA && data[k+1]==indexB) {
+					coincidencias-=2;
+					i++;
+					break;
+				}else if (data[k-1]==indexC && data[k]==indexA && k>0) {
+					coincidencias-=1;
+				}
+			}
+		}
+		if (coincidencias!=0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+//Notese que no puedo llamar directamente a mi funcion cargarDisponibilidad,
+//en cambio debe primero llamar a una jQuery
+$("#cargar_disponibilidad").click(function(){
+	cargarDisponibilidad();
+	//falta algo como recargar pagina?
+});
