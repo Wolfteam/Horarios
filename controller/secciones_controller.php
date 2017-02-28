c07fff6c88cac0a6870899df49bd2b6efc88c4fc
Efrain Bastidas <mimo4325@gmail.com>
2017-02-27 22:03:25 -0400

    Rework de la logica de la parte del servidor.

--- controller/secciones_controller.php
+++ controller/secciones_controller.php
@@ -2,98 +2,59 @@
 	require($_SERVER['DOCUMENT_ROOT'].'/Horarios/Model/DBConnection.php');
 	require_once($_SERVER['DOCUMENT_ROOT'].'/Horarios/Model/MateriasModel.php');	
 	require_once($_SERVER['DOCUMENT_ROOT'].'/Horarios/Model/SeccionesModel.php'); 
-	$link = DBConnection::connection();
 
-	if ($_POST['operacion']=="materias") {
-	    $obj = new MateriasModel($link);
-	    $result = $obj->getMaterias();
-	    $data = "<option>Seleccione una materia</option>";
-	    foreach ($result as $key) {
-	    	$data.= "<option value=".$key['Codigo'].">Semestre:".$key['Semestre']." ".$key['Asignatura']."</option>";
+	$link = DBConnection::connection();
+	$materias = new MateriasModel($link);
+	$secciones = new SeccionesModel($link);
+
+	if (isset($_POST['operacion']) && $_POST['operacion']=="materias") {
+	    $resultado = $materias->getMaterias();
+	    $data = "<option value='0'>Seleccione una materia</option>";
+	    foreach ($resultado as $key) {
+	    	$data.= "<option value=".$key['codigo'].">Semestre:".$key['semestre']." ".$key['asignatura']."</option>";
 	    }
 	    echo $data;
-	}	
-
-	if ($_POST['operacion']=="create") {
-		if (isset($_POST['numero_seccion']) && isset($_POST['cantidad_alumnos']) && isset($_POST['codigomateria'])) {
-	 
-	    $numeroSeccion = $_POST['numero_seccion'];
-	    $cantidadAlumnos = $_POST['cantidad_alumnos'];
-	    //$codigo = $_POST['codigo'];
-	 	$codigomateria = $_POST['codigomateria'];
-	    $object = new SeccionesModel($link);
-	 
-	    $object->createSecciones($numeroSeccion,$cantidadAlumnos,$codigomateria);
-		}
 	}
 
-	if ($_POST['operacion']=="read") {
-		$object = new SeccionesModel($link);
-		// Design initial table header
-		$data = '<table class="table table-bordered  table-hover text-center">
-		                    <tr class="success">
-		                        <th class="text-center">No.</th>
-		                        <th class="text-center">Codigo</th>
-		                        <th class="text-center">Semestre</th>
-		                        <th class="text-center">Asignatura</th>    
-		                        <th class="text-center">Numero Seccion</th>
-		                        <th class="text-center">Cantidad Alumnos</th>                            
-		                        <th class="text-center">Update</th>
-		                        <th class="text-center">Delete</th>
-		                    </tr>';
-		$users = $object->getSecciones();
-		if (count($users) > 0) {
-			$number = 1;
-			foreach ($users as $user) {
-				//Nota como usa el .= para concatener cada fila
-			    $data.= "<tr>
-					<td>".$number."</td> 
-					<td>".$user['Codigo']."</td>
-					<td>".$user['Semestre']."</td>
-					<td>".$user['Asignatura']."</td>
-					<td>".$user['Numero_Seccion']."</td>
-					<td>".$user['Cantidad_Alumnos']."</td>
-					<td> <button onclick='GetUserDetails(".$user['ID'].")' class='btn btn-warning'>Update</button> </td>
-					<td> <button onclick='DeleteUser(".$user['ID'].")' class='btn btn-danger'>Delete</button> </td>
-					</tr>";
-			    $number++;
-			}
+	if (isset($_POST['operacion']) && $_POST['operacion']=="create" && isset($_POST['codigo']) && isset($_POST['cantidad_alumnos']) && isset($_POST['cantidad_secciones']) && $_POST['codigo']!="" && $_POST['cantidad_alumnos']!="" && $_POST['cantidad_secciones']!="") {
+	    $codigo = $_POST['codigo'];
+	    $cantidadSecciones = $_POST['cantidad_secciones'];
+	    $cantidadAlumnos = $_POST['cantidad_alumnos'];
+	    $secciones->createSecciones($codigo,$cantidadSecciones,$cantidadAlumnos);
+	}
 
+	if (isset($_POST['operacion']) && $_POST['operacion']=="read") {
+		$data = '<table class="table table-bordered table-hover table-condensed">
+		                    <tr class="success">
+		                        <th >Codigo</th>
+		                        <th >Semestre</th>
+		                        <th >Asignatura</th>    
+		                        <th >Numero Seccion</th>
+		                        <th >Cantidad Alumnos</th>                            
+		                        <th >Borrar</th>
+		                    </tr>';
+		$dato = $secciones->getSecciones();
+		if (count($dato) > 0) {
+			foreach ($dato as $key) {
+			    $data.= "<tr>
+					<td>".$key['codigo']."</td>
+					<td>".$key['semestre']."</td>
+					<td>".$key['asignatura']."</td>
+					<td>".$key['numero_seccion']."</td>
+					<td>".$key['cantidad_alumnos']."</td>
+					<td> <button onclick='deleteStuff(".$key['codigo'].",".$key['numero_seccion'].")' class='btn btn-danger'>Borrar</button> </td>
+					</tr>";
+			}
 		} else {
-		// records not found
-			$data .= '<tr><td colspan="6">Records not found!</td></tr>';
+			$data .= '<tr><td colspan="6">No se encontraron secciones!</td></tr>';
 		}
 		$data .= '</table>';
 		echo $data;
 	}
 
-	if ($_POST['operacion']=="details") {
-		if (isset($_POST['id']) && isset($_POST['id']) != "") {
-		    $id = $_POST['id'];
-		    $object = new SeccionesModel($link);	 
-		    echo $object->getSeccion2($id);
-		}
-
-	}
-
-	if ($_POST['operacion']=="update") {
-		if (isset($_POST)) {		 
-		    $id = $_POST['id'];
-		    $numeroSeccion = $_POST['numero_seccion'];
-		    $cantidadAlumnos = $_POST['cantidad_alumnos'];
-		 
-		    $object = new SeccionesModel($link);
-		 
-		    $object->setSecciones($id,$numeroSeccion,$cantidadAlumnos);
-		}
-	}
-
-
-	if ($_POST['operacion']=="delete") {
-		if (isset($_POST['id']) && isset($_POST['id']) != "") {
-		    $id = $_POST['id'];
-		    $object = new SeccionesModel($link);
-		    $object->deleteSecciones($id);
-		}
+	if (isset($_POST['operacion']) && $_POST['operacion']=="delete" && isset($_POST['codigo']) && isset($_POST['numero_seccion']) && $_POST['codigo']!="" && $_POST['numero_seccion']!="") {
+	    $codigo = $_POST['codigo'];
+	    $numeroSeccion = $_POST['numero_seccion'];
+	    $secciones->deleteSecciones($codigo,$numeroSeccion);
 	}
 ?>
